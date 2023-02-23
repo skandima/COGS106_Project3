888124cc6b3b8773f275ca1461a04147c1e29016
skandima <114192829+skandima@users.noreply.github.com>
2023-02-22 16:17:33 -0800

    fixed issues on line 9 and 11

---
+++ SignalDetection_Refactored.py
@@ -0,0 +1,44 @@
+#Import needed library
+import scipy.stats as stats
+
+#Implement class based on signal detection theory
+class SignalDetection:
+    #Class constructor
+    def __init__(self, hits, misses, falseAlarms, correctRejections):
+        #signal detection theory variables
+        self.hits = hits 
+        self.misses = misses
+        self.falseAlarms = falseAlarms 
+        self.correctRejections = correctRejections
+        #Note: hits and false alarms only effected as they are the numerator and denominator in the rate equations
+
+    #class method to get the hit rate: hits/total signal trials
+    def hit_rate(self):
+        return self.hits / (self.hits + self.misses)
+
+    #class method to get the false alarm rate: false alarms/total noise trials
+    def falseAlarm_rate(self):
+        return self.falseAlarms / (self.falseAlarms + self.correctRejections)
+
+    #class method to calculate d': Z(H) - Z(FA), Z = stats.norm.ppf
+    def d_prime(self):
+        #get hit rate
+        hit_rate = self.hit_rate()
+        falseAlarm_rate = self.falseAlarm_rate()
+        #get z values
+        z_hit = stats.norm.ppf(hit_rate) 
+        z_falseAlarm = stats.norm.ppf(falseAlarm_rate)
+        #return d'
+        return z_hit-z_falseAlarm
+
+    #class method to get the criterion: -0.5*(Z(H) + Z(FA)), Z = stats.norm.ppf
+    def criterion(self):
+        #get hit rates
+        hit_rate = self.hit_rate()
+        falseAlarm_rate = self.falseAlarm_rate()
+        #get Z values
+        z_hit = stats.norm.ppf(hit_rate)
+        z_falseAlarm = stats.norm.ppf(falseAlarm_rate)
+        #return criterion
+        return -0.5*(z_hit + z_falseAlarm)
+
