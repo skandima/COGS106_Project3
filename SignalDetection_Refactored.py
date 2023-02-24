6726716f2ec93fb5a544cdad4cd1ff26272ffb76
skandima <114192829+skandima@users.noreply.github.com>
2023-02-23 19:22:02 -0800

    added SDT plot

--- SignalDetection_Refactored.py
+++ SignalDetection_Refactored.py
@@ -55,7 +55,31 @@ class SignalDetection:
         plt.title('Receiver Operating Characteristic (ROC) curve')
         plt.show()
 
+    def plot_sdt(self, d = 1):
+        hit_mu = 0
+        hit_sigma = 1
+        fa_mu = self.d_prime()
+        fa_sigma = 1
+        x = np.linspace(-4, 4, 500)
+        hit_pdf = stats.norm.pdf(x, hit_mu, hit_sigma)
+        fa_pdf = stats.norm.pdf(x, fa_mu, fa_sigma) 
+        fig, ax = plt.subplots()
+        ax.plot(x, hit_pdf, 'r', label='Signal')
+        ax.plot(x, fa_pdf, 'g', label='Noise')
+        ax.axvline((self.d_prime()/2)+self.criterion(), linestyle='--', color='b', label = "criterion")
+        ax.hlines(max(hit_pdf), self.d_prime(), 0, colors='k', linestyles='dashed', label = "d-prime")
+        ax.set_xlabel('Evidence')
+        ax.set_ylabel('Probability Density')
+        ax.set_title('Signal Detection Theory (SDT) Plot')
+        ax.legend()
+        plt.show()
+
+
+
 sd = SignalDetection(10, 30, 10, 5)
 sd.plot_roc()
+sd.plot_sdt()
+
+
 
 
